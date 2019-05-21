aec9ae58c029f04e4c5cc65b29c59d95febac35b
Hudson8811 <hudson88@yandex.ru>
2019-05-21 16:03:32 +0300

    anestetic, inner, partners

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 $(function() {
-$('.btn11').click(function() {
-     $(this).toggleClass("open");
-     $(".menu").toggleClass("active");
-});
+  $('.btn11').click(function() {
+       $(this).toggleClass("open");
+       $(".menu").toggleClass("active");
+  });
 });
 
-
+if (window.innerWidth < 651) {
+  $('.mobile_partner_slider').owlCarousel({
+      items:1,
+      loop:true,
+      margin:0,
+      nav:true,
+      navText:['<', '>'],
+      dots:false,
+      responsive:{
+              0:{
+                  items:1
+              },
+              450:{
+                  items:2
+              },
+              600:{
+                  items:3
+              }
+          }
+  });
+}
 
 
 
